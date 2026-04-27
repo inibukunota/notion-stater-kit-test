c39a17497f7e57e30d72ec0bcbfbd30a674715ef
inibukunota <inibukunota@gmail.com>
2026-04-28 07:42:59 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,11 +2,11 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '5dceae615f27827bb3940105504988c7',
+  rootNotionPageId: 'a48eae615f27837ea25f013972f59834',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: 'a48eae615f27837ea25f013972f59834',
+  rootNotionSpaceId: null,
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
